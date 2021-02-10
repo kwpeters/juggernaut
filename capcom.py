2029f41539bf4e0e298dbb2d4283ea801b686f59
Kevin Peters <kwpeters@ra.rockwell.com>
2021-02-10 13:16:39 -0500

    Added opening of current project notes.

--- capcom.py
+++ capcom.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os.path
 import editor
 import todo
 import captlog
@@ -14,8 +15,13 @@ if __name__ == '__main__':
     todoFile = todo.TodoLauncher().GetFilePath();
     clipPaletteFile = clippalette.ClipPalette().GetFilePath();
     captlogFile = captlogLauncher.GetFilePath();
-    notesFolder = notes.NotesLauncher().GetFolder()
+    notesFolder = notes.NotesLauncher().GetFolder();
+
+    homeDir = os.getenv('HOME');
+    lemansCommonServicesFilePath = os.path.abspath(os.path.join(homeDir, "OneDrive - Rockwell Automation, Inc", "home", "rok_data", "projects", "lemans", "common services", "common_services.org"));
+    lemansCommonServicesFilePath = '"' + lemansCommonServicesFilePath + '"';
+
 
     # Put most frequently used files last so they will be on the top
     # of the buffer stack.
-    editor.Open(' '.join([clipPaletteFile, notesFolder, captlogFile, todoFile]), False)
+    editor.Open(' '.join([clipPaletteFile, notesFolder, lemansCommonServicesFilePath, captlogFile, todoFile]), False)
